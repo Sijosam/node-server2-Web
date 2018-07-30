ff30fb74eef6e66929d00eef4283b79feec92008
Sijo Sam <sijosam33@gmail.com>
2018-07-30 17:14:03 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url} `;
+  console.log(log);
+  fs.appendFile('serverlog.txt',log + '\n' , (err) => {
+    if(err){
+      console.log('unable to append');
+    }
+  });
+  next();
+});
+
+// app.use((req,res,next) => {
+//     res.render('maintnce.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
+
+
+app.get('/', (req,res) => {
+  res.render('home.hbs', {
+    name:'sijo',
+    message:'WELCOME TO OUR WEBSITE & have a nice time ',
+    likes : [
+      'bike',
+      'cars'
+    ]
+  });
+});
+
+app.get('/about', (req,res) => {
+  res.render('about.hbs',{
+    pageTitle: 'Sijo details',
+
+  });
+})
+
+app.get('/bad', (req,res) => {
+  res.send({
+    error : 'unable to find the message'
+  });
+
+});
+
+app.get('/maintnce', (req,res) => {
+  res.render('maintnce.hbs');
+});
+
+
+
+app.listen(3000, () => {
+  console.log('server started');
+});
